1eda0f4814265469dec94e6ab4c2410e91d00e78
kaziFahmid <nirjhorislam42@gmail.com>
2023-07-17 15:42:56 +0600

    server token env added

--- index.js
+++ index.js
@@ -5,9 +5,49 @@ app.use(express.json())
 app.use(cors())
 require('dotenv').config()
 var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7zs7lw.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
