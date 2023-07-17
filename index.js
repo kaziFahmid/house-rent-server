fd668bc1dc17284abc7fc8e2da7d8a2a7a1db736
kaziFahmid <nirjhorislam42@gmail.com>
2023-07-17 15:27:16 +0600

    server-added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||5000
+app.use(express.json())
+app.use(cors())
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
