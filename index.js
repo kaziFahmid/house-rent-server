ff1d981a44f85ac1aaedf9891a05c2b9d4140d34
unknown <nirjhorislam42@gmail.com>
2023-08-06 17:16:47 +0600

    vercel updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7zs7lw.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
